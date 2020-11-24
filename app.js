3b8a35f7217ba541e6d74bafa5bd54e2428d5113
Vincent Le Dû <vincent.ledu@gmail.com>
2020-11-24 17:24:24 +0100

    first functionnal version

--- app.js
+++ app.js
@@ -3,33 +3,43 @@ const ASSET_TYPE = {
   NEW: 1,
 };
 
-const NOTARY_PERCENT_OLD = 7.9;
+const NOTARY_PERCENT_OLD = 7.8;
 const NOTARY_PERCENT_NEW = 3.6;
 
 const app = new Vue({
   el: "#app",
   data: {
-    net_seller_price: 300000,
-    agency_fees: 0,
+    net_seller_price: 650750,
+    agency_fees: 32250,
     asset_type: ASSET_TYPE.OLD,
 
-    main_loan_tx: 1.35,
-    main_loan_duration: 240,
+    main_loan_tx: 1.2,
+    main_loan_duration: 300,
 
-    relay_loan: 0,
+    relay_loan: 1,
     relay_loan_tx: 1.35,
     relay_loan_duration: 24,
-    asset_sell_price: 100000,
+    asset_sell_price: 410000,
+    percent_sell_price: 70,
 
-    personnal_money: 0,
+    personnal_money: 200000,
     income: 5500,
-    other_expense: 300,
+    other_expense: 0,
+
+    percent_insurance_mortgagor: 100,
+    tx_insurance_mortgagor: 0.2,
+    percent_insurance_comortgagor: 100,
+    tx_insurance_comortgagor: 0.15,
   },
   computed: {
     indebtment_rate: function() {
       return (
         Math.round(
-          ((this.monthly_payment + this.other_expense) / this.income) *
+          ((this.monthly_payment +
+            this.other_expense +
+            this.mortgagor_monthly_payment_insurance_main_loan +
+            this.comortgagor_monthly_payment_insurance_main_loan) /
+            this.income) *
             100 *
             100
         ) / 100
@@ -47,25 +57,83 @@ const app = new Vue({
         : NOTARY_PERCENT_NEW;
     },
     notary_fees: function() {
-      return (this.net_seller_price * this.notary_fees_percent) / 100;
+      return Math.round(this.net_seller_price * this.notary_fees_percent) / 100;
     },
     main_loan_amount: function() {
-      return (
-        parseFloat(this.net_seller_price) -
-        parseFloat(this.personnal_money) +
-        parseFloat(this.notary_fees) -
-        parseFloat(this.relay_loan_amount) +
-        parseFloat(this.agency_fees)
-      );
+      let amount =
+        parseFloat(this.net_seller_price) +
+        parseFloat(this.notary_fees) +
+        parseFloat(this.agency_fees) -
+        parseFloat(this.personnal_money);
+      if (this.relay_loan == 1) {
+        amount -= parseFloat(this.relay_loan_amount);
+      }
+      return amount;
     },
     relay_loan_amount: function() {
-      return Math.round(this.asset_sell_price * 70) / 100;
+      return Math.round(this.asset_sell_price * this.percent_sell_price) / 100;
     },
     relay_loan_monthly_payment: function() {
-      return (this.relay_loan_amount * this.relay_loan_tx) / 100 / 12;
+      return (
+        Math.round(
+          ((this.relay_loan_amount * this.relay_loan_tx) / 100 / 12) * 100
+        ) / 100
+      );
     },
     max_monthly_payment: function() {
-      return (this.income - this.other_expense) * 0.33;
+      return Math.round((this.income - this.other_expense) * 33) / 100;
+    },
+    mortgagor_monthly_payment_insurance_main_loan: function() {
+      return (
+        Math.round(
+          ((this.main_loan_amount *
+            this.tx_insurance_mortgagor *
+            this.percent_insurance_mortgagor) /
+            100 /
+            100 /
+            12) *
+            100
+        ) / 100
+      );
+    },
+    comortgagor_monthly_payment_insurance_main_loan: function() {
+      return (
+        Math.round(
+          ((this.main_loan_amount *
+            this.tx_insurance_comortgagor *
+            this.percent_insurance_comortgagor) /
+            100 /
+            100 /
+            12) *
+            100
+        ) / 100
+      );
+    },
+    mortgagor_relay_loan_monthly_payment_insurance: function() {
+      return (
+        Math.round(
+          ((this.relay_loan_amount *
+            this.tx_insurance_mortgagor *
+            this.percent_insurance_mortgagor) /
+            100 /
+            100 /
+            12) *
+            100
+        ) / 100
+      );
+    },
+    comortgagor_relay_loan_monthly_payment_insurance: function() {
+      return (
+        Math.round(
+          ((this.relay_loan_amount *
+            this.tx_insurance_comortgagor *
+            this.percent_insurance_comortgagor) /
+            100 /
+            100 /
+            12) *
+            100
+        ) / 100
+      );
     },
   },
 });
